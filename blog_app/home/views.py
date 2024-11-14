acd1507c7fb18cf21f0921e1621cceb26adf261a
Youssef <youssefahmed.main@gmail.com>
2024-11-14 17:31:09 +0200

    edeting the URLS method to be friendly with browsers

--- blog_app/home/views.py
+++ blog_app/home/views.py
@@ -7,8 +7,12 @@ def posts_list(request):
     posts = Post.objects.all()
     return render(request , 'home.html',{'posts':posts})
 
-def post_details(request,id):
-    post = get_object_or_404(Post, id=id )
+def post_details(request, year, month, day, post):
+    post = get_object_or_404(Post, 
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
     return render(request , 'details.html',{'post':post})
 
 # views.py
