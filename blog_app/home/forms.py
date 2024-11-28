caf8bca516fcb5cae2f09b49e7eaff38dc2b2d3d
Youssef <youssefahmed.main@gmail.com>
2024-11-28 21:12:15 +0200

    How to share the posts to E-mails

--- blog_app/home/forms.py
+++ blog_app/home/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from .models import *
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=40)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
