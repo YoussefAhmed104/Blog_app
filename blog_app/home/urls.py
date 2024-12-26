49beafed047c9119b80f71cd22719f20e150c955
Youssef <youssefahmed.main@gmail.com>
2024-12-26 17:10:03 +0200

    Hot to improve CEO how to do xml file and add site map

--- blog_app/home/urls.py
+++ blog_app/home/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_details,name='details'),
     path('add/', views.add_post, name='add_post'),
     path('delete/<int:id>/', views.delete_post, name='delete_post'),
-    path('update/<int:id>',views.update_post,name = 'update_post'),
-    path('<int:post_id>/share/', views.share_post, name= 'share_post'),
-    path('<int:post_id>/comment/', views.comment_post, name= 'comment_post'),
+    path('update/<int:id>/',views.update_post,name = 'update_post'),
+    path('share/<int:post_id>/', views.share_post, name= 'share_post'),
+    path('comment/<int:post_id>/', views.comment_post, name= 'comment_post'),
     path('tag/<slug:tag_slug>/', views.posts_list, name='posts_by_tag'),
 ]
 
